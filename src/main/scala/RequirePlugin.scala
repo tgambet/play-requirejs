5bcbf487b1c3a4d5341b83699f031f5b11194c76
Thomas Gambet <thomas.gambet@gmail.com>
2013-06-18 16:32:19 +0200

    removed some keys

--- src/main/scala/RequirePlugin.scala
+++ src/main/scala/RequirePlugin.scala
@@ -9,37 +9,30 @@ object RequirePlugin extends Plugin {
   object RequireJS {
     val folder    = SettingKey[String]("play-requirejs-folder")
     val assets    = SettingKey[PathFinder]("play-requirejs-assets")
-    val options   = SettingKey[Seq[String]]("play-requirejs-options")
-    val output    = SettingKey[File]("play-requirejs-output-directory")
-    val source    = SettingKey[File]("play-requirejs-source-directory")
     val buildFile = SettingKey[File]("play-requirejs-build-file")
-    val build     = TaskKey[Unit]("play-requirejs-build")
+    val buildTask = TaskKey[Unit]("play-requirejs-build")
     lazy val baseSettings = requireBaseSettings
   }
 
   import RequireJS._
 
+  private val options = SettingKey[Seq[String]]("play-requirejs-options")
+
   val requireBuildTask = (
     folder,
+    buildFile,
     resourceManaged in Compile,
     classDirectory in Compile,
-    source,
-    output,
-    buildFile,
-    baseDirectory,
-    cacheDirectory,
     streams,
-    copyResources in Compile) map { (folder, rm, classes, source, output, buildFile, base, cache, s, cp) =>
-
-    //cp
+    copyResources in Compile) map { (folder, buildFile, resources, classes, s, _) =>
 
     if (!buildFile.exists) {
       s.log.error("Require.js build file not found, expected: " + buildFile)
     } else {
       RequireCompiler.compile(
         sourceBuild = buildFile,
-        targetBuild = rm / "public" / "build.js",
-        sourceDir = rm / "public" / folder,
+        targetBuild = resources / "public" / "build.js",
+        sourceDir = resources / "public" / folder,
         targetDir = classes / "public" / folder
       )
     }
@@ -55,20 +48,17 @@ object RequirePlugin extends Plugin {
   )
 
   lazy val requireBaseSettings = Seq (
-    options := Seq.empty,
     folder := "javascripts-require",
     assets <<= (sourceDirectory in Compile, folder)((sources, folder) => sources / "assets" / folder ** "*"),
-    source <<= (resourceManaged in Compile, folder)((resources, folder) => resources / "public" / folder),
-    output <<= (classDirectory  in Compile, folder)((classes, folder) => classes / "public" / folder),
     buildFile <<= baseDirectory(_ / "project" / "build.js"),
-    build <<= requireBuildTask,
+    buildTask <<= requireBuildTask,
+
+    options := Seq.empty,
     resourceGenerators in Compile <+= requireResourceGenerator,
     javascriptEntryPoints <<= (javascriptEntryPoints, sourceDirectory in Compile, folder)(
       (entryPoints, sources, folder) => (entryPoints --- (sources / "assets" / folder ** "*"))
     ),
     (packageBin in Compile) <<= (packageBin in Compile).dependsOn(requireBuildTask)
   )
-
-
 
 }
